d4a4e51518788789f2349cedac3c346d7e522289
raeesapps <raees.aamir@yandex.com>
2021-10-08 13:54:24 +0100

    Database: implement toString() as yaml serialisation

--- model/src/main/java/Database.java
+++ model/src/main/java/Database.java
@@ -1,6 +1,11 @@
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableList;
+import org.yaml.snakeyaml.Yaml;
 
 public record Database(ImmutableMap<String, ImmutableList<DatabaseObject>> objectLists) {
-
+    @Override
+    public String toString() {
+        var yaml = new Yaml();
+        return yaml.dump(this);
+    }
 }
