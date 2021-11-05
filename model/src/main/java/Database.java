d41d8155e8b3528336c17a0533e88d393b0f0404
raeesapps <raees.aamir@yandex.com>
2021-11-05 15:13:44 +0000

    Database: introduce Builder class

--- model/src/main/java/Database.java
+++ model/src/main/java/Database.java
@@ -1,9 +1,36 @@
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableList;
+import org.javatuples.Pair;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.nodes.Tag;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public record Database(ImmutableMap<String, ImmutableList<DatabaseObject>> objectLists) {
+    public static class Builder {
+        private final Map<String, ImmutableList.Builder<DatabaseObject>> objectListsBuilder = new HashMap<>();
+
+        public Builder object(String objectGroup, DatabaseObject.Builder object) {
+            if (objectListsBuilder.containsKey(objectGroup)) {
+                var objectListBuilder = objectListsBuilder.get(objectGroup);
+                objectListBuilder.add(object.build());
+            } else {
+                objectListsBuilder.put(objectGroup, new ImmutableList.Builder<DatabaseObject>().add(object.build()));
+            }
+
+            return this;
+        }
+
+        public Database build() {
+            var objectListsKeySet = objectListsBuilder.keySet();
+            var objectLists = objectListsKeySet
+                    .stream()
+                    .map(objectType -> Pair.with(objectType, objectListsBuilder.get(objectType).build()))
+                    .collect(new ImmutableMapCollector<>());
+            return new Database(objectLists);
+        }
+    }
     @Override
     public String toString() {
         var yaml = YamlHelper.newYaml();
--- model/src/test/java/DatabaseTests.java
+++ model/src/test/java/DatabaseTests.java
@@ -1,33 +1,28 @@
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class DatabaseTests {
     @Test
     public void database_ToString_ProducesValidYaml() {
-        var databaseObjects = new ImmutableMap.Builder<String, ImmutableList<DatabaseObject>>()
-                .put("tables", new ImmutableList.Builder<DatabaseObject>().add(new DatabaseObject
+        var database = new Database
+                .Builder()
+                .object("tables", new DatabaseObject
                         .Builder()
                         .name("employees")
                         .objectType("table")
                         .dependency(new Dependency(DependencyType.SUBOBJECT, "firstname"))
-                        .dependency(new Dependency(DependencyType.SUBOBJECT, "surname"))
-                        .build()).build())
-                .put("columns", new ImmutableList.Builder<DatabaseObject>().add(
-                        new DatabaseObject
-                                .Builder()
-                                .name("firstname")
-                                .objectType("column")
-                                .property("type", "varchar")
-                                .build(),
-                        new DatabaseObject
-                                .Builder()
-                                .name("surname")
-                                .objectType("column")
-                                .property("type", "varchar")
-                                .build()).build());
-        var database = new Database(databaseObjects.build());
+                        .dependency(new Dependency(DependencyType.SUBOBJECT, "surname")))
+                .object("columns", new DatabaseObject
+                        .Builder()
+                        .name("firstname")
+                        .objectType("column")
+                        .property("type", "varchar"))
+                .object("columns", new DatabaseObject
+                        .Builder()
+                        .name("surname")
+                        .objectType("column")
+                        .property("type", "varchar"))
+                .build();
 
         var serialisation = database.toString();
 
